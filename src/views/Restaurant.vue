e649954221bf4d0c6eda72697252701270055adb
Delta22627 <64527768+Delta22627@users.noreply.github.com>
2021-07-21 19:09:52 +0700

    Add files via upload
    Update the user and restaurant

--- src/views/Restaurant.vue
+++ src/views/Restaurant.vue
@@ -1,38 +1,47 @@
 <template>
+  <v-container>
+    <div class="about">
+      <h1>This is an about page ttt</h1>
+    </div>
+  </v-container>
+  Add User rating
+  <v-rating
+    color="warning"
+    empty-icon="$mdiStarOutline"
+    full-icon="$mdiStar"
+    half-icon="$mdiStarHalfFull"
+    half-increments
+    hover
+    length="5"
+    size="64"
+    value="3"
+  ></v-rating>
 
+  Read only average rating
+  <v-rating
+    v-model="averageRating"
+    color="warning"
+    empty-icon="$mdiStarOutline"
+    full-icon="$mdiStar"
+    half-icon="$mdiStarHalfFull"
+    half-increments
+    hover
+    length="5"
+    readonly
+    size="64"
+    value="3"
+  ></v-rating>
 </template>
 
 <script>
 export default {
-  name: "Restaurant"
+  name: "Restaurant",
+  data: () => ({
+    averageRating: 4,
+  }),
 };
 
-// // Add User rating
-// <v-rating
-//   color="warning"
-//   empty-icon="$mdiStarOutline"
-//   full-icon="$mdiStar"
-//   half-icon="$mdiStarHalfFull"
-//   half-increments
-//   hover
-//   length="5"
-//   size="64"
-//   value="3"
-// ></v-rating>
 
-//Read only average rating
-// <v-rating
-//   color="warning"
-//   empty-icon="$mdiStarOutline"
-//   full-icon="$mdiStar"
-//   half-icon="$mdiStarHalfFull"
-//   half-increments
-//   hover
-//   length="5"
-//   readonly
-//   size="64"
-//   value="3"
-// ></v-rating>
 </script>
 
 <style scoped>
