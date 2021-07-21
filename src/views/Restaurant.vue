db7e3c5fd08cfb5ecbf68faae4f709df6d8d599f
Delta22627 <64527768+Delta22627@users.noreply.github.com>
2021-07-21 18:33:57 +0700

    Add files via upload
    Add template for user and restaurant page

---
+++ src/views/Restaurant.vue
@@ -0,0 +1,40 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  name: "Restaurant"
+};
+
+// // Add User rating
+// <v-rating
+//   color="warning"
+//   empty-icon="$mdiStarOutline"
+//   full-icon="$mdiStar"
+//   half-icon="$mdiStarHalfFull"
+//   half-increments
+//   hover
+//   length="5"
+//   size="64"
+//   value="3"
+// ></v-rating>
+
+//Read only average rating
+// <v-rating
+//   color="warning"
+//   empty-icon="$mdiStarOutline"
+//   full-icon="$mdiStar"
+//   half-icon="$mdiStarHalfFull"
+//   half-increments
+//   hover
+//   length="5"
+//   readonly
+//   size="64"
+//   value="3"
+// ></v-rating>
+</script>
+
+<style scoped>
+
+</style>
