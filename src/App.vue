99b0db89b26e2bf5238ff5ec7cc80542cf5c4dee
Delta22627 <64527768+Delta22627@users.noreply.github.com>
2021-07-21 19:27:48 +0700

    Add files via upload

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,55 @@
 <template>
-  <v-app>
-    <v-app-bar app color="primary" dark>
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
+  <v-app id="inspire">
+    <v-navigation-drawer
+      v-model = "drawer"
+      app
+    >
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title class="text-h6">
+            Restaurant Reviewer
+          </v-list-item-title>
+          <v-list-item-subtitle>
+            Menu
+          </v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
 
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
+      <v-divider></v-divider>
 
-      <v-spacer></v-spacer>
+      <v-list
+        dense
+        nav
+      >
+        <v-list-item
+          v-for="item in items"
+          :key="item.title"
+          :to="item.to"
+          link
+        >
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
 
-      <v-btn href="" target="_blank" text>
-        <span class="mr-2">{{ $store.state.name }}</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+
+
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar app>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+
+      <v-toolbar-title>
+        MUIC Restaurant Reviewer
+      </v-toolbar-title>
     </v-app-bar>
 
     <v-main>
-      <router-view />
+      <router-view></router-view>
     </v-main>
   </v-app>
 </template>
@@ -40,7 +59,14 @@ export default {
   name: "App",
 
   data: () => ({
-    //
+    drawer: null,
+    items: [
+      { title: 'User', icon: 'mdi-account', to:'/user' },
+      { title: 'Home', icon: 'mdi-home', to:'/' },
+      { title: 'About', icon: 'mdi-information',  to:'/about'},
+    ],
+    right: null,
   }),
-};
+}
+
 </script>
