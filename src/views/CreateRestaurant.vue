8e887e1d0f3cec67c9b863ae51c06e638706ae88
Delta22627 <64527768+Delta22627@users.noreply.github.com>
2021-07-21 22:44:54 +0700

    Add files via upload
    add to do pages

---
+++ src/views/CreateRestaurant.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  name: "CreateRestaurant"
+};
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/EditRestaurant.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  name: "EditRestaurant"
+};
+</script>
+
+<style scoped>
+
+</style>
