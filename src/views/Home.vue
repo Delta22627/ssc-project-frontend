80611f675a0f2107c0faa6a4b7d1dd9835f01a51
Delta22627 <user@localhost>
2021-07-22 19:10:00 +0700

    Edit home page to be fit with backend data

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,19 +1,59 @@
 
 <template>
   <v-container>
-    <h2>Home Page</h2>
-    <ul>
-      <li>
-        <router-link to="/">Go to Home</router-link>
-      </li>
-      <li>
-        <router-link to="/about">Go to About</router-link>
-      </li>
-      <li>
-        <router-link to="/login">Go to Login</router-link>
-      </li>
-    </ul>
-
+<!--    <h2>Home Page</h2>-->
+<!--    <ul>-->
+<!--      <li>-->
+<!--        <router-link to="/">Go to Home</router-link>-->
+<!--      </li>-->
+<!--      <li>-->
+<!--        <router-link to="/about">Go to About</router-link>-->
+<!--      </li>-->
+<!--      <li>-->
+<!--        <router-link to="/login">Go to Login</router-link>-->
+<!--      </li>-->
+<!--    </ul>-->
+    <v-row>
+      <v-col
+          v-for="n in 9"
+          :key="n"
+          class="d-flex child-flex"
+          cols="4"
+      >
+        <v-img
+            :src="`https://picsum.photos/500/300?image=${n * 5 + 10}`"
+            :lazy-src="`https://picsum.photos/10/6?image=${n * 5 + 10}`"
+            aspect-ratio="1"
+            class="grey lighten-2"
+        >
+          <template v-slot:placeholder>
+            <v-row
+                class="fill-height ma-0"
+                align="center"
+                justify="center"
+            >
+              <v-progress-circular
+                  indeterminate
+                  color="grey lighten-5"
+              ></v-progress-circular>
+            </v-row>
+          </template>
+        </v-img>
+        <v-rating>
+          v-model ="averageRating"
+          color ="amber"
+          empty-icon="$mdiStarOutline"
+          full-icon="$mdiStar"
+          half-icon="$mdiStarHalfFull"
+          half-increments
+          hover
+          length="5"
+          readonly
+          size="64"
+          value="3"
+          ></v-rating>
+      </v-col>
+    </v-row>
   </v-container>
 
 
@@ -22,8 +62,11 @@
 <script>
 export default {
   name: 'Home',
-
+  data: () => ({
+    averageRating: 4,
+  }),
   components: {
+
   },
 }
 </script>
